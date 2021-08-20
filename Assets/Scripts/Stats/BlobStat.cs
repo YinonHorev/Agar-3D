9589bc21eb6e6d01b7c8a74b093d04e97466f1be
Yinon <inon178@gmail.com>
2021-08-20 17:55:03 +0300

    added player stats and on Collision

--- Assets/Scripts/Stats/BlobStat.cs
+++ Assets/Scripts/Stats/BlobStat.cs
@@ -1,8 +1,51 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class BlobStat : MonoBehaviour
 {
-    public Stat Size;
+	[SerializeField]
+	private readonly float r_SlowDivider;
+
+	[SerializeField]
+	private Stat m_Size;
+
+	[SerializeField]
+	private Stat m_Speed;
+
+	public void AddSize(float i_Size)
+    {
+		m_Size.AddModifier(i_Size);
+		m_Speed.DivideModifier(r_SlowDivider);
+	}
+
+	public float GetSize()
+	{
+		return m_Size.GetValue();
+	}
+
+	public void AddSpeed(float i_Size)
+	{
+		m_Size.AddModifier(i_Size);
+	}
+
+	public float GetSpeed()
+	{
+		return m_Size.GetValue();
+	}
+
+	public bool TryToKill(float i_Damage)
+	{
+        // If Blob has more damage reaches zero
+        if (m_Size.GetValue() - i_Damage <= 0)
+		{
+			return true;
+		}
+
+		return false;
+	}
+
+	public virtual void Die()
+	{
+		// Die in some way
+		Debug.Log(transform.name + " died.");
+	}
 }
