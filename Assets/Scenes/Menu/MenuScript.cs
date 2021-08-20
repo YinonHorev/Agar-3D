1b550f7321e9b3ddb8129a210fdf5fab43665e7c
Yinon <inon178@gmail.com>
2021-08-20 10:41:56 +0300

    initial commit

---
+++ Assets/Scenes/Menu/MenuScript.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject m_HowToPlayImage;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_HowToPlayImage.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Start_OnClick()
+    {
+        SceneManager.LoadScene("Game");
+    }
+    
+    public void HowToPlay_OnClick()
+    {
+        m_HowToPlayImage.SetActive(true);
+    }
+    
+    public void Exit_OnClick()
+    {
+        Application.Quit();
+    }
+    
+    public void BackToMenu_OnClick()
+    {
+        m_HowToPlayImage.SetActive(false);
+    }
+}
