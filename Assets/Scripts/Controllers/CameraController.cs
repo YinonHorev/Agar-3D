192a46b5a165be92fafe45c1667c9b3620dc11af
Yinon <inon178@gmail.com>
2021-08-28 19:11:00 +0300

    fixed camera issue

--- Assets/Scripts/Controllers/CameraController.cs
+++ Assets/Scripts/Controllers/CameraController.cs
@@ -2,19 +2,19 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public GameObject m_Player;
+    public GameObject player;
 
     private Vector3 offset;
     // Start is called before the first frame update
     void Start()
     {
-        offset = transform.position - m_Player.transform.position;
+        offset = transform.position - player.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 p = new Vector3(0,1,-1) * PlayerManager.instance.Player.GetComponent<Transform>().localScale.y;
-        transform.position = m_Player.transform.position + offset + p;
+        Vector3 playerScale = new Vector3(0,1,-1) * PlayerManager.instance.Player.GetComponent<Transform>().localScale.y;
+        transform.position = player.transform.position + offset +  playerScale;
     }
 }
